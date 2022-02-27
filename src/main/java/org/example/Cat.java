ed37f864cba17712d7372262872a4f0467863cbd
crazyrun123456 <464334456@qq.com>
2022-02-27 23:31:45 +0800

    idea中添加cat类，增加colore

---
+++ src/main/java/org/example/Cat.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @CLassName: Cat
+ * @Author： 46433
+ * @Date： 2022/2/27 23:20
+ * @Description TODO
+ */
+public class Cat {
+    private String catName;
+    private String color;
+}
