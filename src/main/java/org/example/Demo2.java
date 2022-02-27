aa73fd6fc956e8086c4d5aa1735087515c97241e
crazyrun123456 <464334456@qq.com>
2022-02-27 23:41:44 +0800

    idea上增加Demo1和Demo2

---
+++ src/main/java/org/example/Demo2.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @CLassName: Demo2
+ * @Author： 46433
+ * @Date： 2022/2/27 23:40
+ * @Description TODO
+ */
+public class Demo2 {
+    private String name; // add in idea
+}
---
+++ src/main/java/org/test/Demo1.java
@@ -0,0 +1,11 @@
+package org.test;
+
+/**
+ * @CLassName: Demo1
+ * @Author： 46433
+ * @Date： 2022/2/27 23:38
+ * @Description TODO
+ */
+public class Demo1 {
+    private String name; // add in idea
+}
